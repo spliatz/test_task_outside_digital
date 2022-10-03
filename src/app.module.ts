7868895b6ee53dba346fb42518c2caad75d8aa63
Mac <mac@MacBook-Air-Mac.local>
2022-10-03 17:08:33 +0400

    Добавил возможность развертывания с помощью Docker Compose и PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {TypeOrmConfigService} from "./shared/typeorm/typeorm.service";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({useClass: TypeOrmConfigService}),
+  ],
 })
 export class AppModule {}
